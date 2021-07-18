44753a2bb5225e14e62daca0927f7596e027cadc
User-niomic <user@niomic.com>
2021-07-18 23:14:58 +0700

    TUGAS 27 JAVASCRIPT
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas27.js
@@ -0,0 +1,12 @@
+var a = "No ";
+var b = "rumah saya ";
+var c = 30;
+var d = "A";
+output1 = a + b + c + d;
+
+var e = "Jalan pangeran Tirtaya No";
+var f = 70;
+output2= e + f;
+
+console.log(output1);
+console.log(output2);
